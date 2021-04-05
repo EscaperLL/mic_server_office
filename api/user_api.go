2d1bdb6a4fe40f0fddafa68e54bda1398c0d01ea
ubuntu_1_0 <1031600551@qq.com>
2021-04-05 08:21:49 -0700

    [MOD] 1、暴露api接口供外部调用

--- api/user_api.go
+++ api/user_api.go
@@ -1,21 +1,36 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"github.com/micro/go-micro/v2"
 
-	//"github.com/micro/go-micro"
-	api "github.com/micro/go-micro/v2/api/proto"
 	//"github.com/micro/go-micro/v2/client"
+	_ "github.com/micro/go-micro/v2/registry/etcd"
+	"encoding/json"
 	"log"
-
+"context"
+	api "github.com/micro/micro/api/proto"
 	mic_src "mic_srv_office/proto/mic_srv_office"
 )
 
 type IUser struct {
 	Client mic_src.IUserService
 }
+
+//func (t *IUser)GetUsers(ctx context.Context,req *api.Request,rsp *api.Response) error{
+//	log.Println("get Users API ---------------")
+//	users :=mic_src.Users{}
+//	_,err :=t.Client.GetUsers(ctx,&users)
+//	if err != nil {
+//		return err
+//	}
+//	rsp.StatusCode=200
+//	user_bytes,_ := json.Marshal(users)
+//	body ,_ := json.Marshal(map[string]string{
+//		"users":string(user_bytes[:]),
+//	})
+//	rsp.Body=string(body)
+//	return nil
+//}
 
 func (t *IUser)GetUsers(ctx context.Context,req *api.Request,rsp *api.Response) error{
 	log.Println("get Users API ---------------")
@@ -38,7 +53,7 @@ func main()  {
 		micro.Name("go.micro.api.user"))
 	service.Init()
 	service.Server().NewHandler(
-		&IUser{Client: mic_src.NewIUserService("go.micro.srv.users",service.Client())})
+		&IUser{Client: mic_src.NewIUserService("go.micro.src.mic_srv_office",service.Client())})
 	if err :=service.Run();err !=nil {
 		log.Fatal(err)
 	}
